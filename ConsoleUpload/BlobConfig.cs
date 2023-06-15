b318889b0118f76df1425dcf82a2371c281020d3
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-06-15 10:19:13 -0300

    :sparkles: feat upload arquivo via console app

---
+++ ConsoleUpload/BlobConfig.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Storage.Blobs;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace ConsoleUpload
+{
+    public class BlobConfig
+    {
+        //string de conexão do blob storage
+        public string connectionString = "";
+        //nome do container
+        public string containerName = "";
+
+
+        public string ImageUpload(string base64Image)
+        {
+            var fileName = Guid.NewGuid().ToString() + ".jpg";
+
+            var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64Image, "");
+
+            byte[] imageBytes = Convert.FromBase64String(data);
+
+
+            var blobClient = new BlobClient(connectionString, containerName, fileName);
+
+            using(var stream = new MemoryStream(imageBytes))
+            {
+                blobClient.Upload(stream);
+            }
+
+            return blobClient.Uri.AbsoluteUri;
+
+        }
+    }
+}
---
+++ ConsoleUpload/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Storage.Blobs;
+
+namespace ConsoleUpload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upload de arquivos/fotos no Azure blob!");
+
+            BlobConfig blobConfig = new BlobConfig();
+
+            string defaultImage = (@"..\..\..\lua.jpg");
+
+            byte[] bytes = File.ReadAllBytes(defaultImage);
+            string base64Image = Convert.ToBase64String(bytes);
+
+            string result = blobConfig.ImageUpload(base64Image);
+
+            Console.WriteLine("Upload ok: " + result);
+        }
+    }
+}
